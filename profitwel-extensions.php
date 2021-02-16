f3b0c4b9917744f11c463fcbc56f7fa7836fe5f6
Hidetaka Okamoto <info@wp-kyoto.net>
2021-02-16 17:22:06 +0900

    init plugin

---
+++ profitwel-extensions.php
@@ -0,0 +1,83 @@
+<?php
+/*
+ * Plugin Name:     Simple tracker for Profitwell
+ * Plugin URI:      http://wordpress.org/plugins/profitwell-extensions/
+ * Description:     Simple plugin to add a tracking code of Profitwell
+ * Version:         0.0.1
+ * Author:          DigitalCube Inc. 
+ * Author URI:      http://en.digitalcube.jp/
+ * License:         GPL-2.0-or-later
+ * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:     profitwell_extension
+*/
+
+class ProfitwellExtension {
+    function __construct() {
+		add_action( 'admin_init', array( $this, 'init_options' ) );
+		add_action( 'wp_footer',  array( $this, 'add_profitwell_script_to_footer' ) );
+    }
+
+	public function init_options() {
+		register_setting( 'reading', 'profitwell_public_api_token', array(
+			'type' => 'string',
+			'sanitize_callback' => 'esc_attr'
+		) );
+		add_settings_section(
+			'profitwell_settings',
+			__( 'Profiltwell', 'profitwell_extension' ),
+			array( $this, 'profitwell_setting_description' ),
+			'reading',
+		);
+		add_settings_field(
+			'profitwell_public_api_token',
+			__( 'Public api token', 'profitwell_extension' ),
+			array( $this, 'profitwell_public_api_token' ),
+			'reading',
+			'profitwell_settings',
+		);
+	}
+	public function profitwell_setting_description() {
+		_e( 'Put the `data-pw-auth` property', 'profitwell_extension' );
+		echo "<pre><code>&lt;script id=\"profitwell-js\" data-pw-auth=\"<b>XXXXXXX</b>\"></pre></code>";
+	}
+
+	public function profitwell_public_api_token() {
+		?>
+		<input
+			id="profitwell_public_api_token"
+			name="profitwell_public_api_token"
+			class="regular-text"
+			type="text"
+			value="<?php form_option('profitwell_public_api_token'); ?>"
+		/>
+		<?php
+	}
+
+	public function add_profitwell_script_to_footer() {
+
+		if ( is_admin() ) {
+			return;
+		}
+
+		$token = esc_attr( get_option( 'profitwell_public_api_token' ) );
+
+		$start_options = "{}";
+		$current_user = wp_get_current_user();
+		if ($current_user->exists()) {
+			$start_options = "{user_email: '{$current_user->user_email}'}";
+		}
+		
+		echo "
+		<script id='profitwell-js' data-pw-auth='$token'>
+			(function(i,s,o,g,r,a,m){i[o]=i[o]||function(){(i[o].q=i[o].q||[]).push(arguments)};
+			a=s.createElement(g);m=s.getElementsByTagName(g)[0];a.async=1;a.src=r+'?auth='+
+			s.getElementById(o+'-js').getAttribute('data-pw-auth');m.parentNode.insertBefore(a,m);
+			})(window,document,'profitwell','script','https://public.profitwell.com/js/profitwell.js');
+
+			profitwell('start', $start_options);
+		</script>
+		";
+	}
+}
+
+new ProfitwellExtension();
---
+++ readme.txt
@@ -0,0 +1,28 @@
+=== Simple tracker for Profitwell ===
+Contributors:      hideokamoto
+Tags:              profitwel
+Requires at least: 5.5
+Tested up to: 5.5.3
+Requires PHP: 7.2
+License:           GPL-2.0-or-later
+License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+
+Simple tracking code inserter for Profitwell.
+
+== Description ==
+
+ProfitWell Retain needs you to add a bit of JavaScript to your application so that it can display in-app notifications to your users, as well as overlay our payment forms on top of your marketing site.
+
+And the plugin can easy to add the snipet into your WordPress site.
+
+== Installation ==
+
+- Install the plugin
+- Go to Settings > Reading page
+- Put your Profitwell public api key
+
+== Changelog ==
+
+
+= 0.1.0 =
+* Release
